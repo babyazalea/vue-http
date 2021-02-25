9db17b980ab5fd907384749e58c0c18b73c22edc
babyazalea <usun12@hotmail.com>
2021-02-25 13:49:18 +0900

    sending POST Request using fetch()

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <learning-survey @survey-submit="storeSurvey"></learning-survey>
+  <learning-survey></learning-survey>
   <user-experiences :results="savedSurveyResults"></user-experiences>
 </template>
 
@@ -10,24 +10,24 @@ import UserExperiences from './components/survey/UserExperiences.vue';
 export default {
   components: {
     LearningSurvey,
-    UserExperiences,
-  },
-  data() {
-    return {
-      savedSurveyResults: [],
-    };
-  },
-  methods: {
-    storeSurvey(surveyData) {
-      const surveyResult = {
-        name: surveyData.userName,
-        rating: surveyData.rating,
-        id: new Date().toISOString(),
-      };
-      this.savedSurveyResults.push(surveyResult);
-      console.log(surveyResult);
-    },
-  },
+    UserExperiences
+  }
+  // data() {
+  //   return {
+  //     savedSurveyResults: [],
+  //   };
+  // },
+  // methods: {
+  //   storeSurvey(surveyData) {
+  //     const surveyResult = {
+  //       name: surveyData.userName,
+  //       rating: surveyData.rating,
+  //       id: new Date().toISOString(),
+  //     };
+  //     this.savedSurveyResults.push(surveyResult);
+  //     console.log(surveyResult);
+  //   },
+  // },
 };
 </script>
 
